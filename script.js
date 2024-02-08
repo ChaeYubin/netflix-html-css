8914eb66a035c82e670e0ff36ec48e34b732fc8a
yubin <cyoobin@naver.com>
2024-02-08 11:25:39 +0900

    script.js 라이트모드 관련 주석 추가

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ document.onload = setInitialTheme(localStorage.getItem("theme"));
 function setInitialTheme(themeKey) {
   if (themeKey === "light") {
     document.documentElement.classList.add("lightTheme");
-    themeToggleButton.classList.toggle("theme_toggle_button_dark");
-    themeToggleButton.classList.toggle("theme_toggle_button_light");
+    classListToggle(); // 처음 로드할 때 dark 클래스로 로드하므로
   } else {
     document.documentElement.classList.remove("lightTheme");
   }
 }
 themeToggleButton.addEventListener("click", () => {
   document.documentElement.classList.toggle("lightTheme");
-  themeToggleButton.classList.toggle("theme_toggle_button_dark");
-  themeToggleButton.classList.toggle("theme_toggle_button_light");
+  classListToggle();
 
   if (document.documentElement.classList.contains("lightTheme")) {
     localStorage.setItem("theme", "light");
@@ -22,3 +20,8 @@ themeToggleButton.addEventListener("click", () => {
     localStorage.setItem("theme", "dark");
   }
 });
+
+function classListToggle() {
+  themeToggleButton.classList.toggle("theme_toggle_button_dark");
+  themeToggleButton.classList.toggle("theme_toggle_button_light");
+}
